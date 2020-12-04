cadf90a9572c608f02560b219bbc4e16dc070ef6
ERGÜN TOPRAK <ergun.35.toprak@gmail.com>
2020-12-04 21:45:02 +0300

    docker change value

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://93.115.79.5:8080/',
-  baseUrl: 'http://93.115.79.5'
+  apiUrl: 'https://api.izmiregitimkurumlari.com',
+  baseUrl: 'https://www.izmiregitimkurumlari.com'
 };
